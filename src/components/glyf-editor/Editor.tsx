98145f7c501ad88d195f48aa5da953add529cc96
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2024-01-09 12:42:39 +0330

    chore: link plugin

--- src/components/glyf-editor/Editor.tsx
+++ src/components/glyf-editor/Editor.tsx
@@ -26,7 +26,55 @@ const theme = {
     strikethrough: 'glyf-editor-strikethrough',
     underlineStrikethrough: 'glyf-editor-underlineStrikethrough'
   },
-  banner: 'glyf-editor-banner'
+  banner: 'glyf-editor-banner',
+  ltr: 'ltr',
+  rtl: 'rtl',
+  placeholder: 'editor-placeholder',
+  paragraph: 'editor-paragraph',
+  quote: 'editor-quote',
+  list: {
+    nested: {
+      listitem: 'editor-nested-listitem'
+    },
+    ol: 'editor-list-ol',
+    ul: 'editor-list-ul',
+    listitem: 'editor-listitem'
+  },
+  image: 'editor-image',
+  link: 'editor-link',
+  code: 'editor-code',
+  codeHighlight: {
+    atrule: 'editor-tokenAttr',
+    attr: 'editor-tokenAttr',
+    boolean: 'editor-tokenProperty',
+    builtin: 'editor-tokenSelector',
+    cdata: 'editor-tokenComment',
+    char: 'editor-tokenSelector',
+    class: 'editor-tokenFunction',
+    'class-name': 'editor-tokenFunction',
+    comment: 'editor-tokenComment',
+    constant: 'editor-tokenProperty',
+    deleted: 'editor-tokenProperty',
+    doctype: 'editor-tokenComment',
+    entity: 'editor-tokenOperator',
+    function: 'editor-tokenFunction',
+    important: 'editor-tokenVariable',
+    inserted: 'editor-tokenSelector',
+    keyword: 'editor-tokenAttr',
+    namespace: 'editor-tokenVariable',
+    number: 'editor-tokenProperty',
+    operator: 'editor-tokenOperator',
+    prolog: 'editor-tokenComment',
+    property: 'editor-tokenProperty',
+    punctuation: 'editor-tokenPunctuation',
+    regex: 'editor-tokenVariable',
+    selector: 'editor-tokenSelector',
+    string: 'editor-tokenSelector',
+    symbol: 'editor-tokenProperty',
+    tag: 'editor-tokenProperty',
+    url: 'editor-tokenOperator',
+    variable: 'editor-tokenVariable'
+  }
 };
 
 function onError(error: Error): void {
--- src/components/glyf-toolbar/Toolbar.tsx
+++ src/components/glyf-toolbar/Toolbar.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
 import './styles.css';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { $setBlocksType_experimental } from '@lexical/selection';
-import { $isRangeSelection, $getSelection, type TextFormatType } from 'lexical';
+import { $isAtNodeEnd, $setBlocksType_experimental } from '@lexical/selection';
+import {
+  $isRangeSelection,
+  $getSelection,
+  type TextFormatType,
+  type LexicalEditor,
+  SELECTION_CHANGE_COMMAND
+} from 'lexical';
+import { mergeRegister } from '@lexical/utils';
 import { $createHeadingNode } from '@lexical/rich-text';
 import { INSERT_ORDERED_LIST_COMMAND, INSERT_UNORDERED_LIST_COMMAND } from '@lexical/list';
+import { TOGGLE_LINK_COMMAND, $isLinkNode } from '@lexical/link';
 import { INSERT_BANNER_COMMAND } from '../glyf-editor/plugins/banner/BannerPlugin';
 import * as Toolbar from '@radix-ui/react-toolbar';
 import {
@@ -12,10 +20,14 @@ import {
   FontBoldIcon,
   FontItalicIcon,
   UnderlineIcon,
-  PlusCircledIcon
+  PlusCircledIcon,
+  ExternalLinkIcon
 } from '@radix-ui/react-icons';
 import { OrderedListIcon, UnorderedListIcon } from './icons';
 import { BannerColorPickerPlugin } from '../glyf-editor/plugins/banner/BannerColorPickerPlugin';
+import { createPortal } from 'react-dom';
+
+const LowPriority = 1;
 
 interface ToolbarButtonProps {
   onClick: React.MouseEventHandler<HTMLButtonElement> | undefined;
@@ -141,13 +153,224 @@ function BannerToolbarPlugin(): JSX.Element {
   );
 }
 
+function getSelectedNode(selection: any): any {
+  const anchor = selection.anchor;
+  const focus = selection.focus;
+  const anchorNode = selection.anchor.getNode();
+  const focusNode = selection.focus.getNode();
+  if (anchorNode === focusNode) {
+    return anchorNode;
+  }
+  const isBackward: boolean = selection.isBackward();
+  if (isBackward) {
+    return $isAtNodeEnd(focus) ? anchorNode : focusNode;
+  } else {
+    return $isAtNodeEnd(anchor) ? focusNode : anchorNode;
+  }
+}
+
+function positionEditorElement(editor: HTMLElement, rect: DOMRect | null): void {
+  if (rect === null) {
+    editor.style.opacity = '0';
+    editor.style.top = '-1000px';
+    editor.style.left = '-1000px';
+  } else {
+    editor.style.opacity = '1';
+    editor.style.top = `${rect.top + rect.height + window.pageYOffset + 10}px`;
+    editor.style.left = `${
+      rect.left + window.pageXOffset - editor.offsetWidth / 2 + rect.width / 2
+    }px`;
+  }
+}
+
+function FloatingLinkEditor({ editor }: { editor: LexicalEditor }): JSX.Element {
+  const editorRef = React.useRef(null);
+  const inputRef = React.useRef<React.ComponentRef<'input'>>(null);
+  const mouseDownRef = React.useRef(false);
+  const [linkUrl, setLinkUrl] = React.useState('');
+  const [isEditMode, setEditMode] = React.useState(false);
+  const [lastSelection, setLastSelection] = React.useState<ReturnType<typeof $getSelection>>(null);
+
+  const updateLinkEditor = React.useCallback(() => {
+    const selection = $getSelection();
+    if ($isRangeSelection(selection)) {
+      const node = getSelectedNode(selection);
+      const parent = node.getParent();
+      if ($isLinkNode(parent)) {
+        setLinkUrl(parent.getURL());
+      } else if ($isLinkNode(node)) {
+        setLinkUrl(node.getURL());
+      } else {
+        setLinkUrl('');
+      }
+    }
+    const editorElem = editorRef.current;
+    const nativeSelection = window.getSelection();
+    const activeElement = document.activeElement;
+
+    if (editorElem === null || nativeSelection === null) {
+      return;
+    }
+
+    const rootElement = editor.getRootElement();
+    if (
+      selection !== null &&
+      !nativeSelection.isCollapsed &&
+      rootElement !== null &&
+      rootElement.contains(nativeSelection.anchorNode)
+    ) {
+      const domRange = nativeSelection.getRangeAt(0);
+      let rect;
+      if (nativeSelection.anchorNode === rootElement) {
+        let inner: Element | HTMLElement = rootElement;
+        while (inner.firstElementChild != null) {
+          inner = inner.firstElementChild;
+        }
+        rect = inner.getBoundingClientRect();
+      } else {
+        rect = domRange.getBoundingClientRect();
+      }
+
+      if (!mouseDownRef.current) {
+        positionEditorElement(editorElem, rect);
+      }
+      setLastSelection(selection);
+    } else if (activeElement === null || activeElement.className !== 'link-input') {
+      positionEditorElement(editorElem, null);
+      setLastSelection(null);
+      setEditMode(false);
+      setLinkUrl('');
+    }
+
+    return true;
+  }, [editor]);
+
+  React.useEffect(() => {
+    return mergeRegister(
+      editor.registerUpdateListener(({ editorState }) => {
+        editorState.read(() => {
+          updateLinkEditor();
+        });
+      }),
+
+      editor.registerCommand(
+        SELECTION_CHANGE_COMMAND,
+        () => {
+          updateLinkEditor();
+          return true;
+        },
+        LowPriority
+      )
+    );
+  }, [editor, updateLinkEditor]);
+
+  React.useEffect(() => {
+    editor.getEditorState().read(() => {
+      updateLinkEditor();
+    });
+  }, [editor, updateLinkEditor]);
+
+  React.useEffect(() => {
+    if (isEditMode && inputRef.current !== null) {
+      inputRef.current.focus();
+    }
+  }, [isEditMode]);
+
+  return (
+    <div ref={editorRef} className="link-editor">
+      {isEditMode ? (
+        <input
+          ref={inputRef}
+          className="link-input"
+          value={linkUrl}
+          onChange={(event) => {
+            setLinkUrl(event.target.value);
+          }}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              event.preventDefault();
+              if (lastSelection !== null) {
+                if (linkUrl !== '') {
+                  editor.dispatchCommand(TOGGLE_LINK_COMMAND, linkUrl);
+                }
+                setEditMode(false);
+              }
+            } else if (event.key === 'Escape') {
+              event.preventDefault();
+              setEditMode(false);
+            }
+          }}
+        />
+      ) : (
+        <>
+          <div className="link-input">
+            <a href={linkUrl} target="_blank" rel="noopener noreferrer">
+              {linkUrl}
+            </a>
+            <div
+              className="link-edit"
+              role="button"
+              tabIndex={0}
+              onMouseDown={(event) => {
+                event.preventDefault();
+              }}
+              onClick={() => {
+                setEditMode(true);
+              }}
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+function LinkToolbarPlugin(): JSX.Element {
+  const [editor] = useLexicalComposerContext();
+  const [isLink, setIsLink] = React.useState(false);
+
+  React.useEffect(() => {
+    editor.getEditorState().read(() => {
+      const selection = $getSelection();
+
+      if ($isRangeSelection(selection)) {
+        const node = getSelectedNode(selection);
+        const parent = node.getParent();
+        if ($isLinkNode(parent) || $isLinkNode(node)) {
+          setIsLink(true);
+        } else {
+          setIsLink(false);
+        }
+      }
+    });
+  }, [editor]);
+
+  const insertLink = (): void => {
+    if (!isLink) {
+      editor.dispatchCommand(TOGGLE_LINK_COMMAND, 'https://');
+    } else {
+      editor.dispatchCommand(TOGGLE_LINK_COMMAND, null);
+    }
+  };
+
+  return (
+    <>
+      <ToolbarButton onClick={insertLink}>
+        <ExternalLinkIcon />
+      </ToolbarButton>
+      {isLink && createPortal(<FloatingLinkEditor editor={editor} />, document.body)}
+    </>
+  );
+}
+
 export function ToolbarPlugin(): JSX.Element {
   return (
     <Toolbar.Root className="toolbarRoot">
       <TextFormatToolbarPlugin />
       <HeadingToolbarPlugin />
       <ListToolbarPlugin />
-      <BannerToolbarPlugin />
+      {false && <BannerToolbarPlugin />}
+      {false && <LinkToolbarPlugin />}
       <BannerColorPickerPlugin />
     </Toolbar.Root>
   );
